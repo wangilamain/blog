353090a65e8e4f5bd4dd0af24ddc8d6b0b31de0a
wangilamain <wangilayng@gmail.com>
2020-09-28 14:54:30 -0400

    page layout

---
+++ start.sh
@@ -0,0 +1,9 @@
+export SECRET_KEY = 'Lovine'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2:/biron:wawira1998@localhost/blogs'
+export MAIL_USERNAME='wangilayng@gmail.com'
+export MAIL_PASS='wawira1998'
+export FLASK_ENV=development
+export DATABASE_URL='postgres://xwccenkgmmyuor:84216b832a80ba0592cb57cce5eb26f5a268a24c89940a003a3de6036e9297f0@ec2-52-202-146-43.compute-1.amazonaws.com:5432/dah5edo1gj4j67'
+
+
+python3 manage.py 
